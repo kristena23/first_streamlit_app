19dec6b028ae868a8fdb7e5abc3f40be27edfdda
kristena23 <147523953+kristena23@users.noreply.github.com>
2023-10-12 15:15:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,6 @@ streamlit.dataframe(my_data_rows)
 
 #new section to display fruitlist api response
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding jackfruit')
+streamlit.write('Thanks for adding jackfruit', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
